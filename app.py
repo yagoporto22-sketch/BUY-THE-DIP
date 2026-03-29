846a99b4f96aa72dd2012f20f88ea7e4a5ab40ca
yagoporto22-sketch <yagoporto22@gmail.com>
2026-03-29 05:13:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ if st.sidebar.button("🚀 Ejecutar Análisis"):
                 if v < -20:
                     return 'background-color: #efb810; color: #ffcccc' # Rojo Pastel
                 elif v > 50:
-                    return 'background-color: #FFD700; color: #000000; 
+                    return 'background-color: #efb810; color: #FFD700'
                 elif v > 20:
                     return 'background-color: #c6efce; color: #006100' # Verde Pastel
                 return ''
